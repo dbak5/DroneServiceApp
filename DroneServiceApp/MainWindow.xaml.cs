74d33cea5bd31ce06826162a03d57d675aa79ee3
dbak5 <dahye.baker@gmail.com>
2023-04-22 13:10:45 +0800

    Add project files.

---
+++ DroneServiceApp/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+// Author: DaHye Baker
+// Student ID: 30063368
+// Organisation: South Metropolitan TAFE
+// Description: Drone Service Application to log drones for service and repair
+
+namespace DroneServiceApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
