1f8aaf231a073effad113a373b300b4f721cddf2
dbak5 <dahye.baker@gmail.com>
2023-04-25 13:20:17 +0800

    setting up

---
+++ DroneServiceApp/Drone.cs
@@ -0,0 +1,96 @@
+﻿// Author: DaHye Baker
+// Student ID: 30063368
+// Organisation: South Metropolitan TAFE
+// Description: Drone Service Application to log drones for service and repair
+
+// Q6.1 Separate class file
+using System.Xml.Linq;
+
+namespace DroneServiceApp
+{
+    internal class Drone
+    {
+        // Q6.2 List of Class
+
+        // Declare variables
+        // ReSharper disable InconsistentNaming
+        private string ClientName;
+        private string DroneModel;
+        private string ServiceProblem;
+        private double ServiceCost;
+        private int ServiceTag;
+
+        // Default constructor
+        public Drone(string clientName, string droneModel, string serviceProblem)
+        {
+            ClientName = clientName;
+            DroneModel = droneModel;
+            ServiceProblem = serviceProblem;
+        }
+
+        // Constructor with all properties
+        public Drone(string newClientName, string newDroneModel, string newServiceProblem, double newServiceCost, int newServiceTag, string clientName, string droneModel, string serviceProblem)
+        {
+            ClientName = newClientName;
+            DroneModel = newDroneModel;
+            ServiceProblem = newServiceProblem;
+            ServiceCost = newServiceCost;
+            ServiceTag = newServiceTag;
+        }
+
+        #region Getters and Setters
+
+        public void SetClientName(string newClientName)
+        {
+            ClientName = newClientName;
+        }
+
+        public string GetClientName()
+        {
+            return ClientName;
+        }
+
+        public void SetDroneModel(string newDroneModel)
+        {
+            DroneModel = newDroneModel;
+        }
+
+        public string GetDroneModel()
+        {
+            return DroneModel;
+        }
+
+        public void SetServiceProblem(string newServiceProblem)
+        {
+            ServiceProblem = newServiceProblem;
+        }
+
+        public string GetServiceProblem()
+        {
+            return ServiceProblem;
+        }
+
+        public void SetServiceCost(double newServiceCost)
+        {
+            ServiceCost = newServiceCost;
+        }
+
+        public double GetServiceCost()
+        {
+            return ServiceCost;
+        }
+
+        public void SetServiceTag(int newServiceTag)
+        {
+            ServiceTag = newServiceTag;
+        }
+
+        public int GetServiceTag()
+        {
+            return ServiceTag;
+        }
+
+        #endregion
+
+    }
+}
--- DroneServiceApp/MainWindow.xaml.cs
+++ DroneServiceApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
 
 // Author: DaHye Baker
 // Student ID: 30063368
@@ -17,24 +18,44 @@ namespace DroneServiceApp
             InitializeComponent();
         }
 
-        // Q6.1 Separate class file
-        // Q6.2 List of Class
-        // Q6.3 Queue of class (regular service
-        // Q6.4 Queue of class (express service)
-        // Q6.5 Add new service item
-        // Q6.6 Increase express service by 15% 
-        // Q6.7 Custom method to return radio button priority
-        // Q6.8 Custom method to display regular service queue in ListView
-        // Q6.9 Custom method to display express service queue in ListView
-        // Q6.10 Keypress method for service cost
-        // Q6.11 Custom method to increment service tag control
-        // Q6.12 Mouse click method to populate textbox from regular service ListView
-        // Q6.13 Mouse click method to populate textbox from express service ListView
-        // Q6.14 Button method to dequeue regular data structure and add item to list
-        // Q6.15 Button method to dequeue express data structure and add item to list
-        // Q6.16 Double click method to remove item from listbox and list data structure
-        // Q6.17 Custom method to clear textboxes
-        // Q6.18 Comments mapping criteria to code and all code meets organisational standards
+        public List<Drone> FinishedList = new();
+        public Queue<Drone> ExpressQueue = new();
+        public Queue<Drone> RegularQueue = new();
 
+        #region Buttons and Events
+        // Q6.10 Keypress method for service cost
+
+        // Q6.12 Mouse click method to populate textbox from regular service ListView
+
+        // Q6.13 Mouse click method to populate textbox from express service ListView
+
+        // Q6.14 Button method to dequeue regular data structure and add item to list
+
+        // Q6.15 Button method to dequeue express data structure and add item to list
+
+        // Q6.16 Double click method to remove item from listbox and list data structure
+
+        #endregion
+
+        #region Methods
+        // Q6.3 Queue of class (regular service)
+
+        // Q6.4 Queue of class (express service)
+
+        // Q6.5 Add new service item
+
+        // Q6.6 Increase express service by 15% 
+
+        // Q6.7 Custom method to return radio button priority
+
+        // Q6.8 Custom method to display regular service queue in ListView
+
+        // Q6.9 Custom method to display express service queue in ListView
+
+        // Q6.11 Custom method to increment service tag control
+
+        // Q6.17 Custom method to clear textboxes
+
+        #endregion
     }
 }
